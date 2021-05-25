9f9e56c2708fcd3d0f484d8785b379ee2e353621
Victor de Oliveira Gianelli <victorgnll@gmail.com>
2021-05-25 16:05:39 -0300

    Continuando com Hooks useEffect

--- ReactNative/1_fundamentos/projeto/myskills/src/pages/home.js
+++ ReactNative/1_fundamentos/projeto/myskills/src/pages/home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import {
  View, Text, StyleSheet, TextInput,
  Platform, FlatList
@@ -9,16 +9,33 @@ import { SkillCard } from '../components/SkillCard';
 export function Home() {
  const [newSkill, setNewSkills] = useState('');
  const [mySkill, setMySkill] = useState([]);
+ const [greeting, setGreeting] = useState('');
 
  function handleAddNewSkill() {
   setMySkill(oldState => [...oldState, newSkill])
-  setNewSkills('')
  }
+
+ useEffect(() => {
+  const currentHour = new Date().getHours()
+  console.log(currentHour)
+
+  if (currentHour >= 18 || currentHour < 5) {
+   setGreeting("Good Night")
+  } else if (currentHour < 12) {
+   setGreeting("Good Morning")
+  } else {
+   setGreeting("Good Afternoon")
+  }
+ }, [mySkill])
 
  return (
   <View style={styles.container}>
 
    <Text style={styles.title}>Welcome, Pessoa!!</Text>
+
+   <Text style={styles.greetings}>
+    {greeting}
+   </Text>
 
    <TextInput
     style={styles.input}
@@ -93,5 +110,9 @@ const styles = StyleSheet.create({
   color: '#fff',
   fontSize: 22,
   fontWeight: 'bold'
+ },
+ greetings: {
+  color: '#fff',
+  fontSize: 12
  }
 })
