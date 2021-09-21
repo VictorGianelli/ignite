ed58cb431e8e927d57cab82bc5aac9aae4d6dc4a
Victor de Oliveira Gianelli <victorgnll@gmail.com>
2021-09-21 20:44:00 -0300

    Utilizando os métodos HTTP

--- Node js/fundamentos-nodejs/src/index.js
+++ Node js/fundamentos-nodejs/src/index.js
@@ -3,8 +3,24 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (request, response) => {
- return response.json({ message: "Hello world" })
+app.get("/course", (request, response) => {
+ return response.json(["Curso 1","Curso 2","Curso 3"])
 })
 
+app.post("/course", (request, response) => {
+ return response.json(["Curso 1","Curso 2","Curso 3","Curso 4"])
+})
+
+app.put("/course/:id", (request, response) => {
+ return response.json(["Curso 6","Curso 2","Curso 3","Curso 4"])
+})
+
+app.patch("/course/:id", (request, response) => {
+ return response.json(["Curso 6","Curso 7","Curso 3","Curso 4"])
+})
+
+app.delete("/course/:id ", (request, response) => {
+ return response.json(["Curso 6","Curso 2","Curso 4"])
+})
+ 
 app.listen(3333)
