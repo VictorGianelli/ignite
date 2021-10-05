40287f2f5a4da944fc5c9a5eb41bc904b1c3cf78
Victor de Oliveira Gianelli <victorgnll@gmail.com>
2021-10-04 22:36:51 -0300

    Configurando ts-node-dev

---
+++ Node js/nodeAPI/src/server.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const app = express();
+
+app.get("/", (requet, response) => {
+ return response.json({ message: "Hello world" })
+})
+
+app.listen(3333, () => console.log("Server running"))
